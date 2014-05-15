edfb053d0db8e6e523b2c564e000b94ecb0119a9
QuimFerrer <qferrer63@gmail.com>
2014-05-15 20:58:47 +0200

    sha acabat el broquil

--- src/pinball.h
+++ src/pinball.h
@@ -8,9 +8,6 @@
             <li><a id="torneig" 	href="../html/torneig.php">Torneigs</a></li>
             <li><a id="contacte"	href="../html/contacte.php">Contactar</a></li>
             <li><a id="usuaris"     href="../html/usuaris.php">Zona de joc</a></li>
-afegeixo texte  ...
-
-
         </ul>
     </div> 
 <?php } ?>
