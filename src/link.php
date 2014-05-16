ce02f59332012c65449fd48ff636c910e7840797
QuimFerrer <quim_ferrer@yahoo.es>
2014-05-16 13:35:00 +0200

    Connexio amb servidor i automatitzacio de productes

---
+++ src/link.php
@@ -0,0 +1,44 @@
+<?php
+	/* 
+	 * Script de connexió a BD remota pinball
+	 * Aquest script s'ha de situar al mateix directori remot on resideix la BD
+	 * Cal donar permisos d'execució i permisos a la BD (drop/create/insert/delete/update)
+	 * Actua com a wraper entre els scripts locals del grup de treball i la connexió a
+	 * la BD remota.
+	 * Parámetres : reb->URL codificada, retorna->resultat de la consulta en JSON
+	 */
+
+	if (!isset($_GET['query']) ) die('<h1>No es una consulta correcte !</h1>');
+
+	$link = mysql_connect('mysql.hostinger.es', 'u555588791_curs', 'calmolins');
+
+	if (!$link)	die('Not connected : ' . mysql_error());
+
+	$db_selected = mysql_select_db('u555588791_pinba', $link);
+
+	if (!$db_selected) {
+	    die ('No es possible utilitzar bd pinball: ' . mysql_error());
+	}
+
+	$query  = urldecode($_GET['query']);
+	$result = mysql_query($query);
+
+	if (!$result) {
+	    die('Consulta no valida: ' . mysql_error());
+	}
+
+	// Obtenir la consulta en forma d'arrays d'objectes
+	$json = array();
+	while ($obj = mysql_fetch_object($result)) {
+	    $json[] = $obj;
+	}
+
+	// Retornar array codificat JSON
+	echo json_encode($json);
+
+	// Alliberar resultat
+	mysql_free_result($result);
+
+	// Tancar connexió
+	mysql_close($link);
+?>
--- src/pinball.h
+++ src/pinball.h
@@ -1,5 +1,25 @@
+<?php function dbExec($query) {
+    /* dbExec. Connecta amb script remot link.php
+     * Executa : string amb consulta SQL
+     * Retorna : string JSON
+     */
+    $ch = curl_init();
+
+    $query = urlencode($query);
+
+    $url = "http://getex.net/cloud/php/link.php?query={$query}";
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($response);
+}?>
+
 <?php function menu() { ?>
-	<div class="menu">
+    <div class="menu">
         <h3>Menu</h3>
         <ul>
             <li><a id="inici"  		href="../html/inici.php">Inici</a></li>
