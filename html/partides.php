cfbb7395dcd57c62e55bb0809ee29ea254d32b0f
VictorAdu <viadusil@gmail.com>
2014-07-07 19:25:09 +0200

    Nou arxiu partides amb AJAX

--- html/partides.php
+++ html/partides.php
@@ -29,83 +29,93 @@ $(document).ready(function(){
 	
 	$.ajax({
 	    url: "query.php",
-	    data: {pid:'5041', idUserPartida:iuserlogin},
-	    success: function(Resultat1){crearmenu1(Resultat1);}});
+	    data: {pid:'6000', idUserPart:iuserlogin},
+	    success: function(Resultat1){crearmenu(Resultat1);}});
 	});
-    
-    
-    function crearmenu1(dadesretornades1){
-	var dadesfiltrades1= JSON.parse(dadesretornades1); 
-	var torneigfiltrats = dadesfiltrades1.records;
+	});
+     
+    function crearmenu(dadesretornades1){
+	var dadesconvert= JSON.parse(dadesretornades1); 
+	
+	var dadesfiltrades = dadesconvert.records;  
+	document.getElementById('informacions').innerHTML = dadesretornades1;  
+ 
+	if (dadesconvert.total>0) {
+	
+	$("#llistat").html('Tria una combinacio torneig/maquina <select id="seleccionador"  name="Combinacio"> </select>'); // label="Combinacio"
+	
+	  var contingut = "";
+	
+	  for (var i=0; i<dadesfiltrades.length; i++){
+	  //console.log("i: " + i);
+	  var esTorneig = dadesfiltrades[i].nomTorn;
+	  var esMaquina = dadesfiltrades[i].idMaq;
+	  var esJoc = dadesfiltrades[i].idJoc;
+	  var esJug = dadesfiltrades[i].idJug;
+	  var esIdTorneig = dadesfiltrades[i].idTorn;
+	  
+	  contingut += '<option id="'+esTorneig+'*'+esMaquina+'" value="'+esTorneig+'*'+esMaquina+'">Torneig: '+esTorneig+' // Maquina: '+esMaquina+'</option>';
+	  //  console.log("esTorneig: " , esTorneig);  // !!!!!!!!!!!!!!!!!!!! COMA ,,,, i NO + ,,,,, !!!!!!!!!!!!!!!!!!!! 
 	 
-	$("#desplegable").html('Tria una maquina <select id="seleccionador" label="Combinacio" name="botomaq"> </select>');
+      	  } // end for
 	
-	var grups = "";
-	if (dadesfiltrades1.total>=0) {
-	  for (var i=0; i<torneigfiltrats.length; i++){
-	  console.log("i: " + i);
-	  var estorneig = torneigfiltrats[i].nomTorn;
-	  var esjoc = torneigfiltrats[i].nomJoc;
-	  document.getElementById('paquetdades2').innerHTML += " / " + estorneig + " ** " + esjoc + " ** " + iuserlogin; 
+	  $("#seleccionador").html(contingut);
+	  document.getElementById('informacions').innerHTML = "";
 	  
+	  console.log(document.getElementsByName('Combinacio').value);
 	  
-	   // for (estorneig in torneigfiltrats[i]){
-	    console.log("estorneig: " + estorneig);
-	    grups += '<optgroup id="'+i+'" label="'+estorneig+'">'+estorneig+'</optgroup>'; 
-	    //+estorneig+'">'+estorneig+
-	    //+idMaq[nomTorn]+'">'+idMaq[nomTorn]+
-	    //+estorneig[nomTorn]+'">'+estorneig[nomTorn]+
-	   //}
-	    document.getElementById('paquetdades2').innerHTML = grups;
-	    $("#seleccionador").html(grups); 
+	} // end if 
 	
-	    $.ajax({
-	    url: "query.php",
-	    data: {pid:'6000', idUserPartida:iuserlogin, novaopcio:estorneig},
-	    success: function(Resultat2){crearmenu2(Resultat2);}});
-	
+	else {
+	document.getElementById('llistat').innerHTML = "No hi ha cap torneig";
+	$("#seleccionador").html("");
+	}
+	  
+	  $('#seleccionador').on('change',function(){
+	    var combinat = $(this).val();
+	    console.log("combinat!" , combinat, "///", "combinat.length" ,   combinat.length); // per veure el contingut amb el separador * 
+	//    console.log(combinat.length);
+	    var seraTorneig = combinat.substring(0,1); 
+	    var seraMaquina = combinat.substring(0,1);
 	    
-	    function crearmenu2(dadesretornades2){
-		    var dadesfiltrades2 = JSON.parse(dadesretornades2);  
-		    var maquinafiltrada = dadesfiltrades2.records; 
-			document.getElementById('paquetdades1').innerHTML = dadesretornades2; // veure al final del docu una copia del resultat
-			console.log("dadesfiltrades2: " , dadesfiltrades2);  // !!!!!!!!!!!!!!!!!!!! COMA ,,,, i NO + ,,,,, !!!!!!!!!!!!!!!!!!!!
-
-		    
-		    var contingut = "";
-		    if (dadesfiltrades2.total>=0) {
-			  for (var j=0; j<maquinafiltrada.length; j++){
-			  //console.log(maquinafiltrada.length);
-			 // document.getElementById('paquetdades3').innerHTML += " / " + maquinafiltrada[j].idMaq; 
-			  console.log("j: " + j);
-
-			      var esmaquina = maquinafiltrada[j].idMaq;
-			      
-				//for (idMaq in maquinafiltrada[j]){
-				console.log("esmaquina: " + esmaquina);
-				
-				console.log("j: " + j);
-				contingut += '<option value="'+esmaquina+'" name="'+esmaquina+'">'+esmaquina+'</option>';
-				//contingut += 'hola';
-				//}
-				
-			    } // end for j
-			    
-			    document.getElementById('paquetdades3').innerHTML = contingut;
-			    $("#"+i+"").html(contingut); // "#"+i+""   '#group'+i+'"'  
-			    
-			    } // end if 
-			    
-	    } // end crearmenu2
-	} // end for i
-	} // end if
-	
-	else document.getElementById('paquetdades2').innerHTML = "No hi ha cap torneig";
+	    var sonPunts1 = "";
+	    var sonPunts2 = "";
+	    var sonPunts3 = "";
+	    
+	    function aleatoris(){
+		var incorrecte=true;
+		while (incorrecte){
+		var a = Math.round(Math.random()*10);   // MAJUSCULES LA M !! .. multiplica el random x10 i arrodoneix (ROUND) el Random a l'enter superior 
+		if (a<10) incorrecte=false;		// comprova si és mejor que 10 i en cas que sigui igual a 10 recalcula el numero
+		} // end while
 		
-    } // end crearmenu1
-     
-   });
-     
+		var incorrecte=true;
+		while (incorrecte){
+		var b = Math.round(Math.random()*10);   // MAJUSCULES LA M !! .. multiplica el random x10 i arrodoneix (ROUND) el Random a l'enter superior 
+		if (b<10) incorrecte=false;		// comprova si és mejor que 10 i en cas que sigui igual a 10 recalcula el numero
+		} // end while
+		
+		var incorrecte=true;
+		while (incorrecte){
+		var c = Math.round(Math.random()*10);   // MAJUSCULES LA M !! .. multiplica el random x10 i arrodoneix (ROUND) el Random a l'enter superior 
+		if (c<10) incorrecte=false;		// comprova si és mejor que 10 i en cas que sigui igual a 10 recalcula el numero
+		} // end while
+		
+	     sonPunts1 = document.getElementById('punt1').value=a*b;
+	     sonPunts2 = document.getElementById('punt2').value=b*c;
+	     sonPunts3 = document.getElementById('punt3').value=a*c;
+	     
+	   };
+	    aleatoris();
+	    
+	    
+	    $.ajax({
+	      url: "partides.php",
+	      data: {carregar:'SI', MAQ:esMaquina, JOC:esJoc, JUG:esJug, FOTO:'NO', PUNTUA1:sonPunts1, PUNTUA2:sonPunts2, PUNTUA3:sonPunts3}, //date("Y-n-j H:i:s")
+	      success: function(Resultat1){console.log('he carregat els valors que s han d enviar');}});
+	    
+	  }); // end jQuery onchange combinacio
+    } // end crearmenu
 </script>
 	 
 </head>
@@ -113,21 +123,19 @@ $(document).ready(function(){
 <body>
   <div id="main">
     <header>
-	    <h1>Marcos, deixa de copiar!!</h1>
+	    <h1>tria</h1>
     </header>
     <menu>
 	    <?php menu(); ?>
     </menu>
     
     <section>
-	<h1>Marcos, deixa de copiar!!</h1>
+	<h1>tria</h1>
 	    
       <?php 
       
-	/*if (isset($_SESSION['emplenat']))  {
+	if (isset($_SESSION['emplenat']))  {
 	      if (($_SESSION['emplenat'])=="NO"){
-		  echo "Hola 1 dins el php </br>";*/
-		  
 		  $query   = 'SELECT _04_loginUsuari FROM usuari;';
 		  $response = dbExec($query)[1];
 		  $_SESSION['resultat']=$response;
@@ -142,61 +150,71 @@ $(document).ready(function(){
 		  //$row = $response->fetch_assoc(); // ->fetch_assoc() Mètode de l'objecte RESULT que em permet accedir al seu contingut
 		  //Fatal error: Call to a member function fetch_assoc() on a non-object in /opt/lampp/htdocs/Projectes/pinball/html/partides.php on line 42
 		  
-		  echo '<form action=" partides.php" method="POST">';
+		  echo '<form action="partides.php?carregar=NO" method="POST">';
 		  echo '<select align="right" id="I_User" name="User">';
 		
 		    foreach($response as $jugador) {
-		    echo '<option value="'.$jugador->_04_loginUsuari .'" name="Jugador">' .$jugador->_04_loginUsuari .'	</option>';
+		    echo '<option value="'.$jugador->_04_loginUsuari .'">' .$jugador->_04_loginUsuari .'</option>';
 		    }
 		  echo '</select></br>'; 
-		  echo '<button type="submit" />Enviar dades a la BB.DD';
+		  echo '<div id="llistat"></div>';
+		  echo 'PUNTUACIONS <div id="puntuacions"> Ronda 1 <input id="punt1" type="number" value="" readonly /> Ronda 2 <input id="punt2" type="number" value="" readonly /> Ronda 3 <input id="punt3" type="number" value="" readonly /> </div>';
+		  echo '</br></br></br> <button type="submit" />Enviar dades a la BB.DD';
 		  echo '</form"></br>'; 
 		      
 		  $_SESSION['emplenat']="SI";
-		// }
+		 } // end IF Emplenat NO
 		  
-	/*	else {
-		  if (($_SESSION['emplenat'])=="SI"){
-		  echo "<p>soc a l'insert</p>";
-		  //var_dump($_SESSION['resultat'])
-		  //array(5) { 
-		  //[0]=> object(stdClass)#1 (1) { ["_04_loginUsuari"]=> string(5) "admin" } 
-		  //[1]=> object(stdClass)#2 (1) { ["_04_loginUsuari"]=> string(4) "joan" } 
-		  //[2]=> object(stdClass)#3 (1) { ["_04_loginUsuari"]=> string(5) "josep" } 
-		  //[3]=> object(stdClass)#4 (1) { ["_04_loginUsuari"]=> string(6) "miquel" } 
-		  //[4]=> object(stdClass)#5 (1) { ["_04_loginUsuari"]=> string(3) "rob" } } 
-	    
-		    $query   = 'INSERT INTO joc VALUES (NULL,"' .$_SESSION['resultat'][3]->_04_loginUsuari .'","descJoc45","imgJoc45",0,NOW(),NULL,NULL);';
-		    $response = dbExec($query)[1];
-		    return $response;
+	      else {
+			$IDMAQ = $_REQUEST['MAQ'];
+			var_dump($IDMAQ);
+			
+			$IDJOC = $_REQUEST['JOC'];
+			$IDJUG = $_REQUEST['JUG'];
+			//$IDDATA = $_REQUEST['DATA'];
+			$IDFOTO = $_REQUEST['FOTO'];
+			$PUNTUA_1 = $_REQUEST['PUNTUA1'];
+			$PUNTUA_2 = $_REQUEST['PUNTUA2'];
+			$PUNTUA_3 = $_REQUEST['PUNTUA3'];
+			 
+		      if (($_REQUEST['carregar'])=="SI"){
+		      
+		      echo "<p>carregant dades a les variables</p>"; 
+		   
+		 //   $opcio = isset($_REQUEST['novaopcio'])  ? $_REQUEST['novaopcio'] : "";
+		      } // end IF SI
+	      
+		      if (($_REQUEST['carregar'])=="NO"){
+			  
+		   // echo "<p>S'ha fet l'insert!</p>"; 
 		    
-		    $_SESSION['emplenat']="NO";
-		  }
-		}
-	  }
-	  
-	else {
-	    echo "Hola 3 dins el php </br>"; 
-	    $_SESSION['emplenat']="NO";
-	    header('location:partides.php');
-	}*/
+			    $query   = 'INSERT INTO joc VALUES (NULL, "'.$PUNTUA_1.'", "'.$PUNTUA_2.'", "'.$IDJUG.'", "'.$PUNTUA_3.'", NOW(), NULL, NULL);'; // "' .$IDMAQ .'","' .$IDJOC .'","' .$IDJUG .'"
+			    $response = dbExec($query)[1];
+			    return $response;
+			      
+			    $_SESSION['emplenat']="NO";
+		    } //end IF NO
+		    
+		 // }//end IF isset
+		
+		} //end ELSE
+	   } // END IF
+    
+	    else {
+		$_SESSION['emplenat']="NO";
+		header('location:partides.php');
+	    } // end Else
 	
 	?>
-	  
     </section>
      
-    <div id="paquetdades1"> paquetdades1: </div> </br></br></br>
-    <div id="paquetdades2"> paquetdades2: </div> </br></br></br>
-    <div id="paquetdades3"> paquetdades3: </div>
-    <div id="desplegable"> desplegable </div>
-    
+    <div id="desplegable"> </div> </br></br></br>
+    <div id="informacions">  </div> </br></br></br>
     <footer>
 	    <?php footer(); ?>
     </footer>
   </div>
-</body>
-	
-	
+</body>	
 </html>
 
 <?php
@@ -205,8 +223,21 @@ if (isset($_POST['entrar'])) controlAcces($_POST["usr"],$_POST["pwd"]);
 function formatarDataHora($dataHora)
 {
 
-}
-/*
-{"total":11,"page":0,"records":[{"idMaq":"10","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1005","nomTorn":"mytorn 2014"},{"idMaq":"10","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1000","nomTorn":"fifa 2014"},{"idMaq":"10","macMaq":"","idJoc":"101","nomJoc":"CARS & GIRLS","idJug":"2","loginUsuari":"joan","idTorn":"1001","nomTorn":"arcade 2014"},{"idMaq":"20","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"},{"idMaq":"30","macMaq":"","idJoc":"101","nomJoc":"CARS & GIRLS","idJug":"2","loginUsuari":"joan","idTorn":"1001","nomTorn":"arcade 2014"},{"idMaq":"30","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"},{"idMaq":"40","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1005","nomTorn":"mytorn 2014"},{"idMaq":"40","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1000","nomTorn":"fifa 2014"},{"idMaq":"40","macMaq":"","idJoc":"101","nomJoc":"CARS & GIRLS","idJug":"2","loginUsuari":"joan","idTorn":"1001","nomTorn":"arcade 2014"},{"idMaq":"40","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"},{"idMaq":"50","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"}]}
+} 
+ /*
+ 	 
+// {"total":11,"page":0,"records":[
+//{"idMaq":"10","macMaq":"","idJoc":"101","nomJoc":"CARS & GIRLS","idJug":"2","loginUsuari":"joan","idTorn":"1001","nomTorn":"arcade 2014"},
+//{"idMaq":"30","macMaq":"","idJoc":"101","nomJoc":"CARS & GIRLS","idJug":"2","loginUsuari":"joan","idTorn":"1001","nomTorn":"arcade 2014"},
+//{"idMaq":"40","macMaq":"","idJoc":"101","nomJoc":"CARS & GIRLS","idJug":"2","loginUsuari":"joan","idTorn":"1001","nomTorn":"arcade 2014"},
+//{"idMaq":"20","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"},
+//{"idMaq":"30","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"},
+//{"idMaq":"40","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"},
+//{"idMaq":"50","macMaq":"","idJoc":"103","nomJoc":"LUCKY FIRE","idJug":"2","loginUsuari":"joan","idTorn":"1003","nomTorn":"castles 2014"},
+//{"idMaq":"10","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1000","nomTorn":"fifa 2014"},
+//{"idMaq":"40","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1000","nomTorn":"fifa 2014"},
+//{"idMaq":"10","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1005","nomTorn":"mytorn 2014"},
+//{"idMaq":"40","macMaq":"","idJoc":"100","nomJoc":"ARMAGEDON","idJug":"2","loginUsuari":"joan","idTorn":"1005","nomTorn":"mytorn 2014"}]}
+
 */
 ?>
